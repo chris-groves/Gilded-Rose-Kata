42deabe3fdfbfca47d3f1b63f19d2fbf91a87069
Chris Groves <chris_groves87@hotmail.co.uk>
2020-02-27 15:16:51 +0000

    Test -backstage pass has 0 quality after event

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -79,5 +79,11 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 13
     end
+
+    it 'decreases the quality to 0 of "Backstage passes to a TAFKAL80ETC concert" after the event' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0
+    end
   end
 end
