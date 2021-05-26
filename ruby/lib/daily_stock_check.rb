399b275f8bb979b9ed5dc671b26bc7ef6ab9e4af
Chris Groves <chris_groves87@hotmail.co.uk>
2021-05-26 18:24:21 +0100

    Refactor updating regular items method

--- ruby/lib/daily_stock_check.rb
+++ ruby/lib/daily_stock_check.rb
@@ -10,7 +10,6 @@ class DailyStockCheck
 
   def update_item(item)
     return item if sulfuras?(item)
-    reduce_item_sell_in_by_one(item)
     update_aged_brie(item) if aged_brie?(item)
     update_backstage_passes(item) if backstage_passes?(item)
     update_regular_item(item) if regular_item?(item)
@@ -19,19 +18,19 @@ class DailyStockCheck
   private
 
   def update_regular_item(item)
-    if item.quality > 0 && item.sell_in >= 0
-      reduce_item_quality_by_one(item)
-    end
-    if item.sell_in < 0 && item.quality > 1
-      reduce_item_quality_by_two(item)
-    end
+    reduce_item_sell_in_by_one(item)
+    return if item.quality == 0
+    reduce_item_quality_by_one(item)
+    reduce_item_quality_by_one(item) if item.sell_in < 0
   end
 
   def update_aged_brie(item)
+    reduce_item_sell_in_by_one(item)
     increase_item_quality_by_one(item) if item.quality < 50
   end
 
   def update_backstage_passes(item)
+    reduce_item_sell_in_by_one(item)
     increase_item_quality_by_three(item) if item.sell_in < 6 && item.quality < 50
     increase_item_quality_by_two(item) if item.sell_in.between?(6,10) && item.quality < 50
     increase_item_quality_by_one(item) if item.sell_in > 10 && item.quality < 50
